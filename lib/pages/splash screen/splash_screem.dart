ca8e378307137a3fd5d1ac6c713ce3df3a261855
Joho06 <jhjaviersartica@gmail.com>
2023-12-14 17:47:14 -0500

    splash style

---
+++ lib/pages/splash screen/splash_screem.dart
@@ -0,0 +1,61 @@
+import 'package:aza_bank/index.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreem extends StatefulWidget {
+  const SplashScreem({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreem> createState() => _SplashScreemState();
+}
+
+class _SplashScreemState extends State<SplashScreem> {
+
+@override
+  void initState() {
+    super.initState();
+    var d = const Duration(seconds: 4);
+    Future.delayed(d,(){
+      Navigator.pushAndRemoveUntil(
+      context as BuildContext,
+      MaterialPageRoute(builder: (context) => WelcomePageWidget()),
+
+      (route) => false);
+});
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Stack(children: [
+      Container(
+      decoration: const BoxDecoration(
+      image: DecorationImage(image: AssetImage("assets/images/azul.png"),
+        fit: BoxFit.cover,
+      )
+      ),
+        child: const Align(
+          alignment: Alignment.bottomCenter,
+          child: ListTile(
+            titleTextStyle: TextStyle(color: Colors.white),
+            title: Text('AzaBanck',style: TextStyle(fontSize: 25),
+              textAlign: TextAlign.center,
+            ),
+            subtitle: Text("Bienvenido a la App",style: TextStyle(fontSize: 25,color: Colors.white),
+              textAlign: TextAlign.center,
+
+            ),
+          ),
+        ),
+      ),
+      const Padding(
+          padding: EdgeInsets.all(90),
+      child: Align(
+        alignment: Alignment.bottomCenter,
+        child: CircularProgressIndicator(
+          color: Colors.indigo,
+        ),
+      ),
+      )
+],),
+    );
+  }
+}
